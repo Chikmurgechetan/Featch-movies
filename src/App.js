5039979bcc4a82f0d69581c55cbddff4fc73e488
Chikmurgechetan <chikmurgechetan@gmail.com>
2023-06-02 18:47:18 +0530

    set aycs await and isLoading

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoding, setIsLoding] = useState(false);
 
-  function fetchMovies() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json(); // Add return statement here
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      })
+  async function fetchMovies() {
+    setIsLoding(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      console.log("Error fetching movies:", error);
+    }
+    setIsLoding(false);
   }
 
   return (
@@ -30,7 +35,9 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoding && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoding && movies.length === 0 && <p>Found No Movies</p>}
+        {isLoding && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
